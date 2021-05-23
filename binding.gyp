2771f629418fa8fd3d2cac8fbb8ce512a8ac6738
Joey J <joey.jaulimsing@live.com>
2021-05-24 03:07:23 +0400

    Initial commit

---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+    "targets" : [
+        {
+        "target_name": "ElectronWallpaper",
+        "sources": ["src/electron-wallpaper.cc"]
+        }
+    ]
+}
---
+++ example/preload.js
@@ -0,0 +1,7 @@
+window.addEventListener('attach', function(){
+    console.log("triggered attach")
+})
+
+window.addEventListener('detach', function(){
+    console.log("triggered detach")
+})
